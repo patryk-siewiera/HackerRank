986463775833409be8b8c037be96d246ee19ee51
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2019-03-22 20:04:33 +0100

    week 4 testing

---
+++ edx/week 4 (Good Programming Practices)/7. Testing and Debugging/notepadTestingDebugging.py
@@ -0,0 +1,29 @@
+"""
+week 4 - testing and debugging
+
+- defensive programming
+	- write specifications for functions
+	- modularize programs
+	- check conditions on inputs / outputs (assertions)
+
+- testing
+	- compare input / output pairs to specification
+	- its not working!
+	- how can i break my program? 
+
+- eliminate source of bugs - debugging 
+	- study events - leading up to an error
+	- why is it not working?
+	- how can I fix my program?
+
+CLASS OF TESTS
+- from the start, design code to ease this part
+- break program into modules that can be tested and debugged individually
+- document constraints on modules
+	- what do you expect the input to be? the output to be?
+- document assumptions behind code design 
+
+
+
+"""
+
