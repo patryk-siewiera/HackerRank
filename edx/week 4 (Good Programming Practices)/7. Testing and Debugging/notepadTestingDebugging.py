4716887f162912416769ef91e9408a2f5b6e5c4c
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2019-03-25 15:48:25 +0100

    bugs topic learning

--- edx/week 4 (Good Programming Practices)/7. Testing and Debugging/notepadTestingDebugging.py
+++ edx/week 4 (Good Programming Practices)/7. Testing and Debugging/notepadTestingDebugging.py
@@ -78,10 +78,123 @@ called PATH-COMPLETE if every potential path through code is tested at least onc
  	- retest until code runs correctly
 
  
+ RUNTIME BUGS:
+ - overt vs convert:
+ 	- OVERT - has a obvious manifestation - code crash or run forever
+ 	- CONVERT - no obvious sign - code return a value, which may be incorrect but hard to determine
+ - Persistent vs intermittent:
+ 	- PERSISTENT - occurs every time code is run
+ 	- INTERMITTENT - only some times 
 
+CATEGORIES OF BUGS:
+- overt and persistent:
+	- obvious to detect
+	- Good programmers use DEFENSIVE PROGRAMMING to try to ensure that if error is made, bug will fall into this category
+- overt and intermittent
+	- more frustrating, can be harder to debug, but if conditions that prompt bug can be reproduced, can be handled
+- convert
+	- highly dangerous, as users may not realize answers are incorrect until code has been run for long period
 
+DEBUGGING
+- steep learning curve
+- goal is to have bug - free program
+- tools:
+	 - build in to IDLE and Anaconda
+	 - Python Tutor
+	 - print statement
+	 - use your brain, be systematic in your hunt 
 
+PRINT STATEMETS:
+- good way to TEST HYPOTHESIS
+- when to print:
+	- enter function
+	- parameters
+	- function results
+- use BISECTION METHOD
+	- put print halfway in code
+	- decide where bug may occur
 
+ERROR MESSAGES -> EASY 
+- IndexError
+	- test = [1,2,3] -> test[4]
+	- trying to access beyond the limits of a list
+- TypeError
+	- int(test)
+	- converting inappropriate type
+- NameError
+	- a
+	- referencing a non-existent variable
+- TypeError
+	- '3'/4
+	- mixing data types without appropriate coercion
+- SyntaxError
+	- a = len([1,2,3] 
+	print a
+	- forgetting to close parenthesis, quotation
+
+>>>>>>>>>>>>>>>>>
+LOGIC ERRORS -> HARDER
+- THINK before writing new code
+- DRAW pictures, take a break
+- EXPLAING the code to 
+>>>>>>>>>>>>>>>>
+
+DEBUGGING STEPS:
+- STUDY program code
+	- ask how did I get the unexpected result
+	- DON'T ASK WHAT IS WRONG
+	- is it part of a family?
+- SCIENTIFIC METHOD
+	- study available data
+	- form hypothesis
+	- repeatable experiments
+	- pick simplest input to test with
+
+DON'T:
+- write entire program
+- test entire program
+- debug entire program
+
+DO:
+- write a function
+- test the function, debug the function
+- write a function
+- test the function, debug the function
+- etc.
+
+DON'T:
+- change code
+- remember where bug was
+- test code
+- FORGET WHERE BUG WAS OR WHAT CHANGE YOU MADE
+- panic :(
+
+DO:
+- backup code
+- change code
+- write down potential bug in a comment
+- test code
+- compare new version with old version
+
+DEBUGGING SKILLS:
+- treat as a search problem:
+	- looking for explanation for incorrect behaviour
+	- study avaliable data - both correct test cases and incorrect ones
+	- form an hypothesis consistent with the data
+	- design and run a repeatable experiment with potential to refute the hypothesis
+	- keep record of experiments performed: use narror range of hypotheses
+
+DEBUGGING AS SEARCH:
+- want to narrow down space of possible sources of error
+- design experiments that expose intermediate stages of computation (use print statements!), and use results to further narrow search
+- binary search can be a powerful tool for this
+
+SOME PRAGMATIC HINTS:
+- look for the usual suspects
+- ask why the code is doing what it is, not why it is not doing what you want
+- the bug is probably not where you think it is - eliminate locations
+- explain the problem to someone else
+- don't belive the documentation
+- take a break and come back to the bug later
 
 """
-
---
+++ edx/week 4 (Good Programming Practices)/8. Exceptions and Assertions/notepadExceptionsAndAssertions.py
@@ -0,0 +1,16 @@
+# notepad exceptions and assertions
+
+"""
+WHAT TO DO WITH EXCEPTIONS?
+- return a "error" value
+	- what value to choose?
+	- complicates code having to check for a special valuie
+- stop execution, SIGNAL ERROR condition
+	 - in python: RAISE AN EXCEPTION
+	 ex -> raise Exception ("descriptive string")
+
+	
+
+
+
+"""
