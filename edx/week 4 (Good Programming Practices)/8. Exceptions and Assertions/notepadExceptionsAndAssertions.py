93730687207eab6ef849735262ab5af033fd9100
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2019-03-25 15:57:35 +0100

    Update notepadExceptionsAndAssertions.py

--- edx/week 4 (Good Programming Practices)/8. Exceptions and Assertions/notepadExceptionsAndAssertions.py
+++ edx/week 4 (Good Programming Practices)/8. Exceptions and Assertions/notepadExceptionsAndAssertions.py
@@ -4,13 +4,24 @@
 WHAT TO DO WITH EXCEPTIONS?
 - return a "error" value
 	- what value to choose?
-	- complicates code having to check for a special valuie
+	- complicates code having to check for a special value
 - stop execution, SIGNAL ERROR condition
 	 - in python: RAISE AN EXCEPTION
 	 ex -> raise Exception ("descriptive string")
 
-	
+DEALING WITH EXCEPTIONS
+Python code can provide HANDLERS for exceptions	
+"""
+try:
+    a = int(input("tell me a number: "))
+    b = int(input("tell me another: "))
+    print(a / b)
+    print("ok")
+except:
+    print("there is bug")
+print("outside")
 
-
+"""
+exceptions RAISED by any statement in body of TRY are HANDLED by the EXCEPT statement and executions continues after the body of the EXCEPT statement 
 
 """
