f65fc698f61471a622da94b974d4dd6deb18bed7
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2019-05-21 12:43:29 +0200

    update

--- HackerRank/Basic Data Types/Nested Lists.py
+++ HackerRank/Basic Data Types/Nested Lists.py
@@ -1,5 +1,10 @@
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-       
+# if __name__ == '__main__':
+#     for _ in range(int(input())):
+#         name = input()
+#         score = float(input())
+
+list = []
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    list.app
