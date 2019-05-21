2a6f79e1b8179b1656d3b08655634dc72732f112
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2019-05-21 12:36:18 +0200

    lists

---
+++ HackerRank/Strings/Capitalize!.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+# TODO im just checking this
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the solve function below.
+def solve(s):
+    pass
+
+
+if __name__ == '__main__':
+    # s = input()
+    s = 'asdad agsfgfd'
+    result = solve(s)
