5ec90a98e661ecba387ae183930ee6f95922de5a
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2019-05-22 12:51:00 +0200

    collections

--- HackerRank/Collections/collectionsNamedtuple.py
+++ HackerRank/Collections/collectionsNamedtuple.py
@@ -17,5 +17,29 @@ def ex2():
     print(xyz.Class)
 
 
-ex1()
-ex2()
+# n = 5
+n = int(input())
+
+l_Id, l_Marks, l_Name, l_Class = ([] for i in range(4))
+l_1, l_2, l_3, l_4 = ([] for j in range(4))
+
+for i in range(n + 1):
+    id1, id2, id3, id4 = input().split()
+    l_1.append(id1)
+    l_2.append(id2)
+    l_3.append(id3)
+    l_4.append(id4)
+
+lista2 = []
+lista2 = [l_1] + [l_2] + [l_3] + [l_4]
+
+for i in range(len(lista2)):
+    if 'MARKS' == lista2[i][0]:
+        l_Marks = (lista2[i][1:])
+        l_Marks = list(map(int, l_Marks))
+
+length = len(l_Marks)
+sums = sum(l_Marks)
+score = sums / length
+score_formatted = '{:2.2f}'.format(score)
+print(score_formatted)
