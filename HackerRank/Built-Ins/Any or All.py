c34772ac159289bd2e5323633de7c79ecb797667
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2019-05-23 10:55:56 +0200

    any, all

--- HackerRank/Built-Ins/Any or All.py
+++ HackerRank/Built-Ins/Any or All.py
@@ -1,23 +1,52 @@
 def ex():
-    print(any([1 > 0, 1 == 0, 1 < 0]))
-    print(all([1 > 0, 1 == 0, 1 < 0]))
+    testList = [1, 2, 3, 4, 5]
+    print(any(i * 2 for i in testList))
+    print(all(i > 0 for i in testList))
 
 
-# ex()
+def solution1():
+    myList = []
+    n = int(input())
+    myList = list(map(int, input().split()))
+    score = True
+    anyScore = False
 
-myList = []
-n = int(input())
-myList = list(map(int, input().split()))
-score = True
+    # TODO 1) all integers must be positive
+    # TODO 2) any integers should be palindrome
 
-for i in range(len(myList)):
-    if myList[i] < 0:
-        score = False
+    for i in range(len(myList)):
+        if myList[i] > 0:  # condition 1: checking if all integers are positive
 
-    word = str(myList[i])
-    word_rev = str(myList[i])[::-1]
+            word = str(myList[i])  # assigning value
+            word_rev = str(myList[i])[::-1]
 
-    if word_rev == word:
-        print("test")
+            if word_rev == word:  # if any of numbers from myList are palidrome
+                anyScore = True
 
-# print(score)
+        elif myList[i] < 0:
+            score = False
+            break
+
+    if score == True and anyScore == True:
+        print(True)
+    else:
+        print(False)
+
+
+def solutionShort():
+    numbersList = []
+    reverseList = []
+
+    n = int(input())
+    numbersList = list(map(int, input().split()))
+
+    isPositive = (all(int(numbersList[l]) > 0 for l in range(len(numbersList))))  # if numbers are positive
+
+    if isPositive == True:
+        [reverseList.append(int((str(numbersList[w]))[::-1])) for w in range(len(numbersList))]  # put in reveseList
+        print(any(numbersList[i] == reverseList[i] for i in range(len(numbersList))))  # check if any is palindrome
+    else:
+        print(False)
+
+
+solutionShort()
