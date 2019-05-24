8455147e74c9baf374bf634a5f899faaa482174b
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2019-05-24 12:17:13 +0200

    regex

--- HackerRank/Regex and Parsing/group groups groupdict.py
+++ HackerRank/Regex and Parsing/group groups groupdict.py
@@ -6,15 +6,14 @@ import re
 # groups() - returns tuple
 # groupdict() - dictionary all named subgroups of the match
 
-word = '..12345678910111213141516171820212223'
+word = '..1234fd5678910111fdd213141516171,820212223'
+#
+# m = re.findall('[0-9]\b+', word)
+# m = ''.join(m)
 
-m = re.findall('[0-9]\b*', word)
-m = ''.join(m)
+regex = re.compile(r'\d+')
+# mo - matching objects
 
-m = re.match('\d', m)
+mo = regex.search(word)
 
-m.groupdict()
-
-# n = re.match()
-
-print(m)
+print(mo.group())
