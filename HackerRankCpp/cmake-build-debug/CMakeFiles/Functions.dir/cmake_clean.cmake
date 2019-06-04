6aa516c752da4ce2e0427c73300a2c5c5dbcf0c2
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2019-06-04 15:29:00 +0200

    cpp

---
+++ HackerRankCpp/cmake-build-debug/CMakeFiles/Functions.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Functions.dir/Introduction/Functions.cpp.obj"
+  "Functions.pdb"
+  "Functions.exe"
+  "Functions.exe.manifest"
+  "libFunctions.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Functions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
